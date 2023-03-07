f2ea424a45e2d7a57f7989116b94d9d196883f69
Mathis <mathis.levieux1@gmail.com>
2023-03-07 15:57:31 +0100

    Navbar refaite

--- views/templates/header.php
+++ views/templates/header.php
@@ -1,35 +1,39 @@
 <header>
 
-    <nav class="navbar navbar-expand-lg p-0">
-        <div class="container-fluid p-0">
-            <button class="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <!-- Navbar -->
+    <nav class="navbar navbar-dark navbar-expand-lg bg-black fixed-top py-4 border-bottom">
+        <div class="container-fluid">
+            <h2 class="mb-0 navbar-brand text-light header-title">TEAM THAI BEN</h2>
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="justify-content-center collapse navbar-collapse" id="navbarSupportedContent">
-
-                <h2 class="title-header">TEAM THAI BEN</h2>
-
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="view-gallery.php">Accueil</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="view-gallery.php">À propos de nous</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="view-gallery.php">Galerie</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="view-calendar.php">Calendrier</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="view-login.php">Tarifs & Infos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="view-signin.php">Adhérer au club</a>
-                    </li>
-                </ul>
-
+            <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Navigation</h5>
+                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body me-0 pe-0 pe-xl-5 me-xl-5">
+                    <ul class="gap-5 navbar-nav justify-content-center flex-grow-1 me-0 pe-0 pe-xl-5 me-xl-5 ul-text-light">
+                        <li class="nav-item">
+                            <a class="nav-link" href="view-gallery.php">Accueil</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="view-gallery.php">À propos de nous</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="view-gallery.php">Galerie</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="view-calendar.php">Calendrier</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="view-login.php">Tarifs & Infos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="view-signin.php">Adhérer au club</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </nav>
