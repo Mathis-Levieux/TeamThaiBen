ba27a930c81afb0480815b5893afb8e59da22669
Mathis <mathis.levieux1@gmail.com>
2023-03-15 15:07:37 +0100

    delete, affichage et téléchargement des fichiers

--- models/files.php
+++ models/files.php
@@ -4,13 +4,26 @@
 class Files
 {
     private object $db;
+    private string $files_name;
+    private int $files_id;
+    private int $files_show;
 
     public function __construct()
     {
         $this->db = Database::connect();
     }
 
-    public function uploadFile($name)
+    public function __get($name)
+    {
+        return $this->$name;
+    }
+
+    public function __set($name, $value)
+    {
+        $this->$name = $value;
+    }
+
+    public function uploadFile($name): void
     {
         $sql = "INSERT INTO sk_files (files_name) VALUES (:name)";
         $query = $this->db->prepare($sql);
@@ -19,12 +32,41 @@ class Files
         ]);
     }
 
-    public function deleteFile($id)
+    public function deleteFile($id): void
     {
         $sql = "DELETE FROM sk_files WHERE files_id = :id";
         $query = $this->db->prepare($sql);
         $query->execute([
             'id' => $id
         ]);
+    }
+
+    public function getAllFiles(): array
+    {
+        $sql = "SELECT * FROM sk_files";
+        $query = $this->db->prepare($sql);
+        $query->execute();
+        $files = $query->fetchAll(PDO::FETCH_ASSOC);
+        return $files;
+    }
+
+    public function getFileById($id)
+    {
+        $sql = "SELECT * FROM sk_files WHERE files_id = :id";
+        $query = $this->db->prepare($sql);
+        $query->execute([
+            'id' => $id
+        ]);
+        $file = $query->fetch(PDO::FETCH_ASSOC);
+        return $file;
+    }
+
+    public function changeFileBoolean($id, $boolean): void
+    {
+        $sql = "UPDATE sk_files SET files_show = :boolean WHERE files_id = :id";
+        $query = $this->db->prepare($sql);
+        $query->bindValue(':boolean', $boolean, PDO::PARAM_BOOL);
+        $query->bindValue(':id', $id, PDO::PARAM_INT);
+        $query->execute();
     }
 }
--- views/view-dashboard-files.php
+++ views/view-dashboard-files.php
@@ -7,28 +7,86 @@
         <div class="container">
             <div class="row h-100">
                 <div class="col-12 justify-content-center">
-
                     <h1>Dashboard - Fichiers</h1>
-
                     <form action='controller-dashboard-files.php' method="post" enctype="multipart/form-data">
-                        <!-- affichage du résultat de l'upload -->
-                        <?php if (!empty($errors)) : ?>
+
+                        <!-- affichage des erreurs -->
+                        <?php if (!empty($_POST) && !empty($errors)) : ?>
                             <div class="alert alert-danger" role="alert">
                                 <?php foreach ($errors as $error) : ?>
                                     <?= $error ?>
                                 <?php endforeach; ?>
                             </div>
                         <?php endif; ?>
-                        <?php if (isset($file) && !empty($file->getSuccessMessage())) : ?>
+                        <!-- fin affichage des erreurs -->
+
+                        <!-- affichage du résultat de l'upload -->
+                        <?php if (!empty($_POST) && isset($file) && !empty($file->getSuccessMessage())) : ?>
                             <div class="alert alert-success"><?php echo $file->getSuccessMessage(); ?></div>
                         <?php endif; ?>
                         <!-- fin affichage du résultat de l'upload -->
+
                         <input name="inputFile" type="file" class="form-control" id="inputFile">
                         <input type="submit" name="submitFile" class="btn btn-primary" value="Envoyer">
                         <div class="h-100 preview-container d-flex flex-wrap gap-2"></div>
                 </div>
             </div>
         </div>
+        </form>
+
+        <div class="container">
+            <div class="row h-100">
+                <div class="col-12 justify-content-center">
+                    <h2>Liste des fichiers</h2>
+                    <!-- Affichage des erreurs -->
+                    <?php if (!empty($_GET) && isset($file) && !empty($file->getErrorsMessage())) : ?>
+                        <div class="alert alert-danger" role="alert">
+                            <?php foreach ($file->getErrorsMessage() as $error) : ?>
+                                <?= $error ?>
+                            <?php endforeach; ?>
+                        </div>
+                    <?php endif; ?>
+                    <!-- Fin affichage des erreurs -->
+
+                    <!-- Affichage des messages de succès -->
+                    <?php if (!empty($_GET) && isset($file) && !empty($file->getSuccessMessage())) : ?>
+                        <div class="alert alert-success"><?php echo $file->getSuccessMessage(); ?></div>
+                    <?php endif; ?>
+                    <!-- Fin affichage des messages de succès -->
+
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Nom du fichier</th>
+                                <th scope="col">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            <!-- Affichage des fichiers -->
+                            <?php $displayFiles = new File();
+                            foreach ($displayFiles->showFiles() as $file) : ?>
+                                <tr>
+                                    <td><?= $file['files_name'] ?></td>
+                                    <td>
+                                        <a href="controller-dashboard-files.php?delete=<?= $file['files_id'] ?>" class="btn btn-danger">Supprimer</a>
+                                        <a href="controller-dashboard-files.php?download=<?= $file['files_id'] ?>" class="btn btn-success">Télécharger</a>
+                                        <?php if ($file['files_show'] == 0) : ?>
+                                            <a href="controller-dashboard-files.php?show=<?= $file['files_id'] ?>" class="btn btn-primary">Afficher</a>
+                                        <?php else : ?>
+                                            <a href="controller-dashboard-files.php?hide=<?= $file['files_id'] ?>" class="btn btn-secondary">Masquer</a>
+                                        <?php endif; ?>
+                                    </td>
+
+
+                                </tr>
+                            <?php endforeach; ?>
+                            <!-- Fin affichage des fichiers -->
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
     </main>
 
 
