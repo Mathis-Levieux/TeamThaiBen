e12c5cc6e0e2bbc444c5c34ce359a37d8b09daa9
Mathis Levieux <mathis.levieux1@gmail.com>
2023-04-04 16:26:58 +0200

    front aboutus

--- views/view-aboutus.php
+++ views/view-aboutus.php
@@ -4,8 +4,26 @@ include('templates/head.php');
 include('templates/header.php');
 ?>
 
-<body>
-    <main>
+<body class="bg-dark text-light">
+    <main class="mb-5">
+        <div class="container">
+            <div class="row">
+                <div class="my-5 col-md-6">
+                    <img src="../assets/img/yellow_group.png" alt="Club Team Thai Ben" class="img-fluid">
+                </div>
+                <div class="col-md-6">
+                    <h1 class="my-5">LE CLUB TEAM THAI BEN</h1>
+                    <h2 class="mt-5 mb-4 thai-font text-center">L'HISTOIRE</h2>
+                    <p> Issu de la boxe anglaise, française, américaine et thaï, Ben enseigne toute
+                        Diplômé de Boxe Thaï en 2014, entraîneur dans de nombreux clubs.
+                        Dans un esprit loisir, familial, proche des élèves pour répondre à la personnalisation et perfectionnement,
+                        Ben met un point d'honneur à prendre soin de chacun, et de faire pratiquer avec patience et pédagogie.
+                        Le président tient au lien de sympathie qui règne entre les membres du club et n'hésite pas à organiser des sorties ou des voyages.
+                        La bonne humeur est de mise, le plaisir est partagé.
+                    </p>
+                </div>
+            </div>
+        </div>
 
     </main>
 
